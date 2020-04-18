b3f54849883e893194b255e629727b4d34dec221
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-18 22:22:26 +0930

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return <Navbar></Navbar>;
+};
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav"></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
