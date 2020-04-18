6b0782a7256f55f41a9907dee8f2794798ffd90c
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-18 23:57:57 +0930

    Added Icons in navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import NavItem from './components/navbar/NavItem';
+
+import { ReactComponent as BellIcon } from './assets/icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './assets/icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './assets/icons/caret.svg';
+import { ReactComponent as PlusIcon } from './assets/icons/plus.svg';
+import { ReactComponent as CogIcon } from './assets/icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './assets/icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './assets/icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './assets/icons/bolt.svg';
 
 const App = () => {
-  return <Navbar></Navbar>;
+  return (
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+    </Navbar>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav"></ul>
+      <ul className="navbar-nav">{props.children}</ul>
     </nav>
   );
 };
